3fdcdc1377e21a753587dd496aeff18852600930
TangLing <1158354636@qq.com>
2018-11-11 09:58:21 +0800

    11.10

---
+++ Test_Static.java
@@ -0,0 +1,20 @@
+public class Test_Static{
+ private static int age=18;
+private String name;
+private double height;
+
+public static void print(){
+	
+	
+	//System.out.println("名字："+name+"年龄"+age+"身高"+height);
+	
+	
+}
+
+public static void main(String[] args){
+	Test_Static.print();
+	
+	System.out.println(Test_Static.name);
+}
+
+}
