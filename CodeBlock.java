5f799aaf26d6db4535f9ef93dab5d3c7bdddd91e
TangLing <1158354636@qq.com>
2018-11-24 15:37:06 +0800

    代码块重练

--- CodeBlock.java
+++ CodeBlock.java
@@ -1,12 +1,28 @@
-public class CodeBlock{
-public static void main(String[] args){
-	
-	
-	{
-		int x=7;
-		System.out.println(x);
+public class CodeBlock extends A{
+	public CodeBlock(){
+	System.out.println("7.B类的构造方法");
 	}
-	int x =5;
-	System.out.println(++x);
+	{
+	System.out.println("6.B类的构造块");
+}
+static{
+	System.out.println("2.B类的静态块");
+}
+public static void main(String[] args){
+	System.out.println("3.Start:");
+	new CodeBlock();
+	System.out.println("8.end");
+}
+
+}
+class A{
+	public A(){
+	System.out.println("5.A类的构造方法");
+}
+{
+	System.out.println("4.A类的构造块");
+}
+static{
+	System.out.println("1.A类的静态块");
 }
 }
