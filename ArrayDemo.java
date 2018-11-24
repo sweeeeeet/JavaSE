2ba7c2f3b877d8b9fb1465729ae17fb09573fe9a
TangLing <1158354636@qq.com>
2018-11-24 15:37:20 +0800

    数组

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -5,75 +5,38 @@ public static void main(String[] args){
 	
 	
 	String langusges[] = new String[]{"汉语","德语","法语","英语"};
-	for(String lang:langusges){
-		
-		System.out.println(lang);
+	int[] data=new int[]{12,222,23,44,55,66};
+	int[] data2=new int[]{1,2,3,4,5,6};
+	int[] newArray=java.util.Arrays.copyOf(data2,data2.length+2);
+	int[] newArray2=java.util.Arrays.copyOf(data2,3);
+
+	printArray(newArray);
+	printArray(newArray2);
+	// char[] chars=new char[]{'n','i','h','a','o'};
+	// System.arraycopy(data2,0,data,1,3);
+	// for(int temp:data){
+		// System.out.println(temp);
+	// }
+	// java.util.Arrays.sort(chars);
+	// for(char temp:chars){
+		// System.out.println(temp);
+	// }
+
+}
+public static void printArray(int[] data){
+	for(int temp:data){
+	System.out.println(temp+"、");
 	}
-
-
-
-	
-	
-	
-	
-// int[] arr=new int[]{1,2,3};
-// printArray(arr);
-
-// int[] data=init();
-// bigger(data);
-// printArray(data);
-
-
-int[] intData=new int[]{1,2,3,4,5};
-char[] charData=new char[]{'l','f','z','t'};
-java.util.Arrays.sort(intData);
-java.util.Arrays.sort(charData);
-
-for(int array:intData){
-	System.out.println(array);
-	
 }
-
-for(char data:charData){
-	System.out.println(data);
-}
-//printArray(charData);
-}
-
-//重载
-public static char[] printArray(char[] data){
-	
+public static int[] modify(int[] data){
 	for(int i=0;i<data.length;i++){
-		
-		System.out.println(data[i]);
-		
+		System.out.println(data[i]*2);
 	}
 	return data;
 }
-public static int[] init(){
-	return new int[]{1,2,3,4};//匿名数组
-	
-}
-
-
-
-public static void bigger(int[] data){
-	
-	for(int i=0;i<data.length;i++){
-		
-		data[i]=data[i]*5;
+public static void printArray(String[] data){
+	for(String temp:data){
+	System.out.println(temp+"、");
 	}
 }
-public static int[] printArray(int[] arr){
-	
-	for(int i=0;i<arr.length;i++){
-		System.out.println(arr[i]);
-		
-	}
-	return arr;
-}
-
-
-
-
 }
