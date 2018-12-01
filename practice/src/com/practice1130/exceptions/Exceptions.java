9c9b5b1766f203951572d91b1cf4d96f9929d3c1
TangLing <1158354636@qq.com>
2018-12-01 10:36:08 +0800

    异常

---
+++ practice/src/com/practice1130/exceptions/Exceptions.java
@@ -0,0 +1,21 @@
+package com.practice1130.exceptions;
+
+/**
+ * Author:sweet
+ * Created:2018/11/30
+ */
+public class Exceptions {
+    public static void main(String[] args) {
+//    int x=0;
+//    int y=0;
+//    int m=x/y;//除数作为0的情况会产生java.lang.ArithmeticException
+//        int arr[]={1,2,3};
+//        System.out.println(arr[3]);//ArrayIndexOutOfBoundsException
+//        boolean b[]={true,true};
+//        int array[]={1,2};
+//        System.arraycopy(array,0,b,0,1);
+//        //数组存入与声明不符，属于RuntimeExceptinon的子类，ArrayStoreException
+
+
+    }
+}
