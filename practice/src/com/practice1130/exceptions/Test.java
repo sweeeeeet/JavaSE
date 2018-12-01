3f1a05b8ac54c3dddb4eaed34b843333d43eab6e
TangLing <1158354636@qq.com>
2018-12-01 10:36:58 +0800

    测试

---
+++ practice/src/com/practice1130/exceptions/Test.java
@@ -0,0 +1,17 @@
+package com.practice1130.exceptions;
+
+/**
+ * Author:sweet
+ * Created:2018/11/30
+ */
+  abstract interface Test {
+     static  void s(){
+
+     }
+
+}
+class Test1{
+      protected interface Nas{
+
+      }
+}
