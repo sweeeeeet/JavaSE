8ccdf598bf4bcfc61fb06f76429de8d017f3a723
TangLing <1158354636@qq.com>
2018-12-06 10:21:18 +0800

    文件类--列出所有文件

---
+++ practice_file/src/com/sweet/ListFileTest.java
@@ -0,0 +1,18 @@
+package com.sweet;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+/**
+ * Author:sweet
+ * Created:2018/12/5
+ */
+public class ListFileTest {
+    public static void main(String[] args) {
+        File file = Paths.get("D:","TL-BITE","Test").toFile();
+        File[] files=file.listFiles();
+        for(File f : files){
+            System.out.println(f);
+        }
+    }
+}
