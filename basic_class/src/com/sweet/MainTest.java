a85d55196285bd614de3fef780c380da95dcfacc
TangLing <1158354636@qq.com>
2018-12-11 18:28:01 +0800

    主方法

---
+++ basic_class/src/com/sweet/MainTest.java
@@ -0,0 +1,14 @@
+package com.sweet;
+
+/**
+ * Author:sweet
+ * Created:2018/12/7
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        for(String arg:args){
+            System.out.println(arg);
+        }
+    }
+}
