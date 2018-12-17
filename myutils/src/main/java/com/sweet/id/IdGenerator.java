b6dc7705c7111c72957ba0c190f67e3d96b42ed1
TangLing <1158354636@qq.com>
2018-12-17 23:50:34 +0800

    1

--- myutils/src/main/java/com/sweet/id/IdGenerator.java
+++ myutils/src/main/java/com/sweet/id/IdGenerator.java
@@ -4,7 +4,7 @@ package com.sweet.id;
  * ID生成器
  * ID相关文章见：http://calvin1978.blogcn.com/articles/uuid.html
  * <p>
- * Author: secondriver
+ * Author: 
  * Created: 2018/10/17
  */
 public interface IdGenerator {
