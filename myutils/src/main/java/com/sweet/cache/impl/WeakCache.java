344e8609dc6da5764e264239d60d8cb39ac010f6
TangLing <1158354636@qq.com>
2018-12-26 15:59:17 +0800

    1

--- myutils/src/main/java/com/sweet/cache/impl/WeakCache.java
+++ myutils/src/main/java/com/sweet/cache/impl/WeakCache.java
@@ -8,7 +8,7 @@ import java.util.WeakHashMap;
  * 弱引用缓存:
  * 对于一个给定的键，其映射的存在并不阻止垃圾回收器对该键的丢弃，这就使该键成为可终止的，被终止，然后被回收。<br>
  * 丢弃某个键时，其条目从映射中有效地移除。
- * Author: secondriver
+ * Author: 
  * Created: 2018/10/16
  */
 public class WeakCache<K, V> extends TimedCache<K, V> {
