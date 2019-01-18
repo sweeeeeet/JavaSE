fc2105e231c0bd4f1aa497014b31fb154c833b43
TangLing <1158354636@qq.com>
2019-01-19 07:42:43 +0800

    反射

---
+++ reflect/src/VO/VO.java
@@ -0,0 +1,40 @@
+package VO;
+
+/**
+ * Author:sweet
+ * Created:2019/1/16
+ */
+ class EMP {
+    private String name;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", job='" + job + '\'' +
+                '}';
+    }
+
+}
+public class VO{
+    public static void main(String[] args) {
+
+    }
+}
