bd7cdebecb0d1041cd34704b390b0042f4b6119c
TangLing <1158354636@qq.com>
2019-01-18 18:49:55 +0800

    utils

---
+++ myutils/src/test/java/com/sweet/util/ArrayUtilTest.java
@@ -0,0 +1,22 @@
+package com.sweet.util;
+
+/**
+ * Author:sweet
+ * Created:2019/1/17
+ */
+public class ArrayUtilTest {
+    public static void main(String[] args) {
+//        int[] num=null;
+//        int[] num1=new int[2];
+//        System.out.println(ArrayUtil.isEmpty(num));;
+//        System.out.println(ArrayUtil.isEmpty(num1));;
+        Object[] objects=new Object[2];
+        Object[] objects1=null;
+        Object object = "String";
+        System.out.println(ArrayUtil.isEmpty(objects));;
+        System.out.println(ArrayUtil.isEmpty(objects1));;
+        System.out.println(ArrayUtil.isEmpty(object));;
+
+
+    }
+}
