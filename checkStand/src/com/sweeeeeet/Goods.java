27bcbadb8df624134da17bb458de41e884ce6505
TangLing <1158354636@qq.com>
2019-01-21 11:58:22 +0800

    收银台++

---
+++ checkStand/src/com/sweeeeeet/Goods.java
@@ -0,0 +1,46 @@
+package com.sweeeeeet;
+
+/**
+ * Author:sweet
+ * Created:2019/1/19
+ */
+public class Goods {
+    private String name;
+    private double price;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Goods(String name, double price, String id) {
+        this.name = name;
+        this.price = price;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%2s] %s %.2f",this.getId(),this.getName(),this.getPrice());
+    }
+}
