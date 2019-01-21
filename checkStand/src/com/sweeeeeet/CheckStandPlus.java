c4f876b28011577f3a6e2202e28cd45dd3567123
TangLing <1158354636@qq.com>
2019-01-21 11:57:59 +0800

    收银台2.0

---
+++ checkStand/src/com/sweeeeeet/CheckStandPlus.java
@@ -0,0 +1,37 @@
+package com.sweeeeeet;
+
+/**
+ * Author:sweet
+ * Created:2019/1/19
+ */
+public class CheckStandPlus {
+    public void helpInfo(){
+        //TODO
+        //帮助信息
+    }
+    public void quit(){
+        //退出逻辑
+    }
+    public void about(){
+        //关于信息
+    }
+    public void usageInfo(){
+        //使用信息
+    }
+    public void settingInfo(){
+        //设置信息
+    }
+    public void usage(){
+        //使用逻辑
+    }
+    public void setting(){
+        //设置逻辑
+    }
+    public void readGoods(){
+        //读取商品
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
