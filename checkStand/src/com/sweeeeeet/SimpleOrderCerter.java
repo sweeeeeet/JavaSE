7d346c5e3369c5edad428de9bdb1da1098e24cbf
TangLing <1158354636@qq.com>
2019-01-22 16:25:17 +0800

    收银台

---
+++ checkStand/src/com/sweeeeeet/SimpleOrderCerter.java
@@ -0,0 +1,40 @@
+package com.sweeeeeet;
+
+import com.sweeeeeet.Order;
+import com.sweeeeeet.OrderCenter;
+
+/**
+ * Author:sweet
+ * Created:2019/1/19
+ */
+public class SimpleOrderCerter implements OrderCenter {
+    @Override
+    public void addOrder(Order order) {
+
+    }
+
+    @Override
+    public void removeOrder(Order order) {
+
+    }
+
+    @Override
+    public String orderTable(String id) {
+        return null;
+    }
+
+    @Override
+    public String ordersTable() {
+        return null;
+    }
+
+    @Override
+    public void storeOrders() {
+
+    }
+
+    @Override
+    public void loadOrders() {
+
+    }
+}
