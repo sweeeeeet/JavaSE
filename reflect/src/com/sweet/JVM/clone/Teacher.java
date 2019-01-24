5c33b2c4a2fe0519652001a9db719c370deb3571
TangLing <1158354636@qq.com>
2019-01-24 09:46:10 +0800

    反射

---
+++ reflect/src/com/sweet/JVM/clone/Teacher.java
@@ -0,0 +1,41 @@
+package com.sweet.JVM.clone;
+
+import java.io.Serializable;
+
+/**
+ * Author:sweet
+ * Created:2019/1/22
+ */
+public class Teacher implements Serializable {
+    private String name;
+    private String direction;
+
+    public Teacher(String name, String direction) {
+        this.name = name;
+        this.direction = direction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", direction='" + direction + '\'' +
+                '}';
+    }
+}
