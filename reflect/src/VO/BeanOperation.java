d927de2775919331d20196e6028dcfa5bc69c694
TangLing <1158354636@qq.com>
2019-01-27 09:56:14 +0800

    反射

--- reflect/src/VO/BeanOperation.java
+++ reflect/src/VO/BeanOperation.java
@@ -1,5 +1,7 @@
 package VO;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -8,41 +10,37 @@ import java.lang.reflect.Method;
  * Created:2019/1/16
  */
 public class BeanOperation {
-    public static void setBeanValue(Object o, String value) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        //1.字符串拆分"emp.name:lihua|emp.job:C"
-        String[] temp = value.split("\\|");
-        //emp.name:lihua|emp.job:C
-        for (int i = 0; i < temp.length; i++) {
-            String[] res = temp[i].split(":");
-            //emp.name:lihua
-            //取得真实值
-            String realvalue = res[1];
-            //取得真实类
-            String realClassName = res[0].split("\\.")[0];
-            //取得属性
-            String attrName = res[0].split("\\.")[1];
-
-            //通过反射取得xxAction中的真实对象
-            Object realObj = getBeanObject(o, realClassName);
-            Class<?> cla=o.getClass();
-            String setName = "set" + toUpper(realClassName);
-            Method method = cla.getMethod(setName);
-            method.invoke(o,realvalue);
+    public static void setBeanValue(Object o,String value) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException, NoSuchFieldException {
+      //  "emp.name:lihua|emp.job:C"
+        //第一次拆分
+        String[] first=value.split("\\|");
+        for(int i=0,len=first.length;i<len;i++){
+            //第二次拆分
+           String[] seconed= first[i].split(":");
+           String setValue=seconed[1];
+           String attribute=seconed[0].split("\\.")[1];
+           String setName="set"+BeanOperation.toUpper(attribute);
+           //利用反射调用方法并set值
+            Field field=o.getClass().getField(setValue);
+            if (field == null) {
+                // 第二次尝试从父类中取得该属性            
+                field = o.getClass().getField(attribute) ;
+            }if (field == null) {
+                // 两次都未取得指定属性的对象，表示该对象一定不存在            
+                return  ;
+            }
+          Method method=  o.getClass().getMethod(setName,field.getType());
+          method.invoke(o,setValue);
+            getBeanObject(o, attribute);
         }
     }
 
-    public static Object getBeanObject(Object o, String realClassName) {
-//取得Action对象的class对象
-        Class<?>cla=o.getClass();
-        // 取对象
-        Object realObj=null;
+    public static Object getBeanObject(Object o, String attribute) {
+        String getName="get"+BeanOperation.toUpper(attribute);
+        Object obj=null;
         try {
-            //拼接get方法名
-        String method="get"+toUpper(realClassName);
-        //获取方法对象
-            Method method1 = cla.getMethod(method);
-            //调用方法
-           realObj= method1.invoke(o);
+            Method method=o.getClass().getMethod(getName);
+         obj=method.invoke(o);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -50,9 +48,11 @@ public class BeanOperation {
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
-        return realObj;
+        return obj;
     }
     public static String toUpper(String value){
         return value.substring(0, 1).toUpperCase() + value.substring(1);
     }
+
+
 }
