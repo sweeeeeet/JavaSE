8bbeda7654f0e98f06d6807ab47f20c37dac390d
TangLing <1158354636@qq.com>
2019-03-13 14:59:34 +0800

    聊天室多线程版

---
+++ chatroom/client/src/com/sweet/chatroom/mulclient/ReadDataFromServer.java
@@ -0,0 +1,12 @@
+package com.sweet.chatroom.mulclient;
+
+/**
+ * Author:sweet
+ * Created:2019/3/12
+ */
+public class ReadDataFromServer extends Thread {
+    @Override
+    public void run() {
+        super.run();
+    }
+}
