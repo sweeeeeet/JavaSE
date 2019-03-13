aec679cfba2dae5916a4cce67aaf59107b9ca2dc
TangLing <1158354636@qq.com>
2019-03-13 15:00:21 +0800

    聊天室多线程版

--- chatroom/client/src/com/sweet/chatroom/mulclient/MulClient.java
+++ chatroom/client/src/com/sweet/chatroom/mulclient/MulClient.java
@@ -1,18 +1,38 @@
 package com.sweet.chatroom.mulclient;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Author:sweet
  * Created:2019/3/9
  */
 public class MulClient {
+    Map<Socket,String> client=new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         try {
             //1.创建Socket
-            Socket client=new Socket("127.0.0.1", 6667);
+            String host="127.0.0.1";
+            Integer port=6667;
+            if(args.length==2){
+           host=args[0];
+           port=Integer.parseInt(args[1]);
+            }
+            Socket client=new Socket(host, port);
+
             //2.通过字符界面交互式输入，把输入的数据发送到服务器
+            OutputStream clientout=client.getOutputStream();
+            OutputStreamWriter clientwrite=new OutputStreamWriter(clientout);
+            String message;
+            Scanner scanner=new Scanner(System.in);
+            message=scanner.nextLine();
+
             //3.读取服务端的数据
         } catch (IOException e) {
             e.printStackTrace();
