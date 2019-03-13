cac8f9b7d53d917c85770d0ed5f1846aaf135b16
TangLing <1158354636@qq.com>
2019-03-13 15:01:48 +0800

    聊天室多线程版

--- chatroom/severlet/src/com/sweet/chatroom/multhreadsever/MulThreadSever.java
+++ chatroom/severlet/src/com/sweet/chatroom/multhreadsever/MulThreadSever.java
@@ -7,6 +7,9 @@ import java.net.Socket;
 /**
  * Author:sweet
  * Created:2019/3/9
+ *
+ * 1.对于可变的数字或值（端口号）不采用硬编码（不写死）：1.main方法中参数传入2.命令行交互式传入3.属性文件
+ *
  */
 public class MulThreadSever {
     public static void main(String[] args) {
@@ -15,9 +18,13 @@ public class MulThreadSever {
             ServerSocket sever=new ServerSocket(6667);
             System.out.println("等待客户端连接");
             //2.接收客户端连接
+            while(true){
            Socket client= sever.accept();
             System.out.println("客户端已连接上服务端，端口号为"+client.getPort());
+
+            }
            //3.通过线程池分发任务
+
 
         } catch (IOException e) {
             e.printStackTrace();
