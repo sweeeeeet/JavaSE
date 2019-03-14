3f591ba56071a2090bfdd3cdce13e32db7e625b3
TangLing <1158354636@qq.com>
2019-03-14 14:53:44 +0800

    聊天室多线程版

--- chatroom/client/src/com/sweet/chatroom/mulclient/MulClient.java
+++ chatroom/client/src/com/sweet/chatroom/mulclient/MulClient.java
@@ -1,5 +1,7 @@
 package com.sweet.chatroom.mulclient;
 
+import com.sun.org.apache.xalan.internal.xsltc.dom.CachedNodeListIterator;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -13,7 +15,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created:2019/3/9
  */
 public class MulClient {
-    Map<Socket,String> client=new ConcurrentHashMap<>();
+
 
     public static void main(String[] args) {
         try {
@@ -21,17 +23,46 @@ public class MulClient {
             String host="127.0.0.1";
             Integer port=6667;
             if(args.length==2){
-           host=args[0];
-           port=Integer.parseInt(args[1]);
+                if(!host.equals(args[0])){
+                    System.out.println("ip非法使用默认host"+host);
+                }else if (!String.valueOf(port).equals(args[1])){
+                    System.out.println("端口号非法，使用默认端口号"+port);
+                }
+
+            }else if(args.length==1){
+                if(!args[0].equals(String.valueOf(port))){
+                    System.out.println("端口号非法，使用默认端口号"+port);
+                }
+            }else{
+                System.out.println("成功连接服务器！");
             }
-            Socket client=new Socket(host, port);
+            //创建用户Socket
+             Socket client=new Socket(host,port);
+
+            System.out.println("输入help查看操作指南");
+           while(true){
+                WriteDataToServer clientWrite=new WriteDataToServer(client);
+           }
+
+
+
+//实现功能
+            /*
+            * 客户端注册：创建Socket给服务端发送注册执行消息
+            * */
+            /*
+            * 客户端群聊功能：客户端发送和接收消息
+            * */
+
+            /*
+            * 客户端私聊，指定客户端用户，能够接收和发送消息
+            * */
+
+            /*
+            * 客户端退出，
+            * */
 
             //2.通过字符界面交互式输入，把输入的数据发送到服务器
-            OutputStream clientout=client.getOutputStream();
-            OutputStreamWriter clientwrite=new OutputStreamWriter(clientout);
-            String message;
-            Scanner scanner=new Scanner(System.in);
-            message=scanner.nextLine();
 
             //3.读取服务端的数据
         } catch (IOException e) {
