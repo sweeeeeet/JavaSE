61a7868f0ee340e70c4b9242bc394c957b6487e7
TangLing <1158354636@qq.com>
2019-03-14 14:53:23 +0800

    聊天室多线程版

---
+++ chatroom/severlet/src/com/sweet/chatroom/multhreadsever/HandlerClient.java
@@ -0,0 +1,15 @@
+package com.sweet.chatroom.multhreadsever;
+
+import java.net.Socket;
+
+/**
+ * Author:sweet
+ *
+ * 读数据写数据，实现客户端的功能
+ * Created:2019/3/13
+ */
+public class HandlerClient {
+   MulThreadSever mulThreadSever;
+    //读数据
+
+}
