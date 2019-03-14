791d6d3c80ac6631ba8066fd2047ad529e6fae86
TangLing <1158354636@qq.com>
2019-03-14 17:40:30 +0800

    聊天室多线程版

--- chatroom/client/src/com/sweet/chatroom/mulclient/WriteDataToServer.java
+++ chatroom/client/src/com/sweet/chatroom/mulclient/WriteDataToServer.java
@@ -4,30 +4,48 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
  * Author:sweet
  * Created:2019/3/12
+ *
+ * 通过字符界面的交互式输入，把输入的数据发送到服务器
  */
 public class WriteDataToServer extends Thread{
-    private  final Socket client ;
-    public WriteDataToServer(Socket client){
-        this.client=client;
+    public  final Socket client;
+
+    public WriteDataToServer(Socket client) {
+       this. client = client;
     }
+
     @Override
     public void run() {
-        OutputStream clientout= null;
+
         try {
-            clientout = client.getOutputStream();
+            OutputStream clientout = client.getOutputStream();
         OutputStreamWriter clientwrite=new OutputStreamWriter(clientout);
-        String message;
         Scanner scanner=new Scanner(System.in);
-        message=scanner.nextLine();
-        clientwrite.write(  message);
+     while(true) {
+         String message;
+         message = scanner.nextLine();
+         clientwrite.write(message + "\n");
+         clientout.flush();
+
+
+//          if (message.startsWith("group")) {
+//             System.out.println("【发送消息给所有用户】" + message.split(":")[1]);
+//         } else if (message.startsWith("privateChat")) {
+//             System.out.println("【发送消息给所有用户】" + message.split(":")[2]);
+//         } else {
+//             System.out.println("【发消息给客户端】" + message);
+//         }
+     }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+
 }
