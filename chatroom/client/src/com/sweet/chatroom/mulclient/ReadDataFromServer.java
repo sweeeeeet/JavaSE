5953a2321f47a1c72ae65b312a9f0ddee96379cd
TangLing <1158354636@qq.com>
2019-03-14 14:54:04 +0800

    聊天室多线程版

--- chatroom/client/src/com/sweet/chatroom/mulclient/ReadDataFromServer.java
+++ chatroom/client/src/com/sweet/chatroom/mulclient/ReadDataFromServer.java
@@ -1,12 +1,33 @@
 package com.sweet.chatroom.mulclient;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Author:sweet
  * Created:2019/3/12
+ *
+ * 读取服务端的数据
  */
 public class ReadDataFromServer extends Thread {
+    public final Socket client;
+    public ReadDataFromServer(Socket client) {
+        this.client = client;
+    }
+
     @Override
     public void run() {
-        super.run();
+        try {
+            InputStreamReader clientread=new InputStreamReader(client.getInputStream())   ;
+        Scanner scanner=new Scanner(clientread);
+        String message=scanner.nextLine();
+        String sendPerson="服务器";
+            System.out.println(sendPerson+"说:"+message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
