87900f0df03e12d3aa5ed8778e0e84f856950c44
TangLing <1158354636@qq.com>
2019-03-28 14:28:11 +0800

    jdbc编程

---
+++ jdbc/src/com/sweet/jdbc/MemoGroup.java
@@ -0,0 +1,56 @@
+package com.sweet.jdbc;
+
+import java.time.LocalDateTime;
+
+/**
+ * Author:sweet
+ * Created:2019/3/25
+ */
+public class MemoGroup {
+    private Integer id;
+    private String name;
+    private LocalDateTime createdTime;
+    private LocalDateTime modifyTime;
+
+    @Override
+    public String toString() {
+        return "MemoGroup{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", createdTime=" + createdTime +
+                ", modifyTime=" + modifyTime +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public LocalDateTime getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(LocalDateTime modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
