5ab983705a3525af47c7ad505fb550ed975e02eb
TangLing <1158354636@qq.com>
2019-03-31 08:50:30 +0800

    JDBC编程

---
+++ jdbc/src/com/sweet/jdbc/UseTemplate3.java
@@ -0,0 +1,26 @@
+package com.sweet.jdbc;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+/**
+ * Author:sweet
+ * Created:2019/3/26
+ */
+public class UseTemplate3 {
+    public static TemplateJDBC3 test = new TemplateJDBC3();
+
+    public static void quiryByName(String name) {
+        String sql = "select id ,name,created_time, modify_time  from memo_group  where name in (?)";
+        List<MemoGroup> memoGroups = test.excute(sql, new String[]{name},(List<MemoGroup> list)->list) ;
+        for(MemoGroup memoGroup:memoGroups){
+            System.out.println(memoGroup);
+        }
+    }
+
+
+    public static void main(String[] args) {
+    quiryByName("java");
+    }
+}
+
