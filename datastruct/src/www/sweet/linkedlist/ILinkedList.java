3e9702f4aabd13f7ace9c563beaf2a51a438b4d7
TangLing <1158354636@qq.com>
2019-03-31 09:11:16 +0800

    顺序链表

---
+++ datastruct/src/www/sweet/linkedlist/ILinkedList.java
@@ -0,0 +1,13 @@
+package www.sweet.linkedlist;
+
+/**
+ * Author:sweet
+ * Created:2019/3/30
+ */
+public interface ILinkedList {
+     void pushFront(int node);
+    void popFront();
+    void pushBack(int node);
+    void popBack();
+
+}
