fc0c7b3551aed3c647a957de4338dc85af4a9115
TangLing <1158354636@qq.com>
2019-04-17 19:19:58 +0800

    Datastructure of the interview question---usage of Stack and Queue

---
+++ datastruct/src/www/sweet/interview/StackAndQueue.java
@@ -0,0 +1,241 @@
+package www.sweet.interview;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * Author:sweet
+ * Created:2019/4/13
+ */
+public class StackAndQueue {
+
+    public static void main(String[] args) {
+        StackAndQueue test = new StackAndQueue();
+        test.isValid("(4+5)+[45]");
+    }
+
+    /**
+     * @Description: 括号匹配
+     * solution：利用栈的先进后出特性，遍历每个字符，将左括号压入栈，
+     * 遍历到右括号时，与栈顶元素匹配
+     * {1.如果栈为空，不匹配
+     * 2.若果右括号与栈顶元素不相等 不匹配
+     * 3.遍历完栈不为空，左括号多了}
+     * @return:
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            switch (ch){
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                case ']':
+                case '}':{
+                    if(stack.empty()){
+                        return false;
+                    }else{
+                        char left=stack.pop();
+                        if(!(left=='(' && ch==')' ||left=='[' && ch==']'
+                                ||left=='{' && ch=='}')){
+                            return false;
+                        }
+                        break;
+                    }
+                }
+                default: break;
+            }
+        }
+
+
+
+        //字符串遍历结束，如果栈中还有元素 说明也不匹配
+        if (!stack.empty()) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    /**
+     * @Description: 用队列（一头进一头出（先进先出）ABC-->ABC）
+     * 实现栈（同一头进出（先进后出）ABC-->CBA）：
+     * solution：private LinkedList www.sweet.queue;
+     * @return:
+     */
+    class MyStack {
+        private LinkedList<Integer> stack = new LinkedList<>();
+
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyStack() {
+
+
+        }
+
+        /**
+         * Push element x onto stack.
+         */
+        public void push(int x) {
+//前插进队列就是栈的实现
+            stack.addFirst(x);
+
+        }
+
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+//出栈:后进先出《----》队列中的最后一个元素
+
+            if(stack.size()!=0){
+
+
+           return stack.remove(0);
+
+            }
+            return 0;
+        }
+
+        /**
+         * Get the top element.
+         */
+        public int top() {
+            //栈顶元素就是后入队列的元素
+            if(stack.size()!=0){
+                return stack.get(0);
+            }
+            return 0;
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         */
+        public boolean empty() {
+            if (stack.size()==0) {
+            return true;
+            }
+            return false;
+        }
+
+    }
+}
+
+
+/**
+ * @Description: 用两个栈实现一个队列
+ * solution：（类似于汉诺塔）出队列：入其中一个栈，
+ * 然后遍历，导入另一个栈，此时的元素顺序就是进栈的顺序了
+ * 若要继续push元素，在第一个栈中添加元素，如果第二个栈还有元素则出，
+ * 如果没有了就再次将第一个栈元素导入第二个元素
+ * @return:
+ * @Description: 最小栈：设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * @Param: solution：利用两个栈：一个栈正常存入数据 另一个栈压入最小的元素（空间换时间）
+ * @return:
+ * @Description: 设计你的循环队列实现。 循环队列是一种线性数据结构，
+ * 其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+ * <p>
+ * 循环队列的一个好处是我们可以利用这个队列之前用过的空间。
+ * 在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。
+ * 但是使用循环队列，我们能使用这些空间去存储新的值。
+ * <p>
+ * 你的实现应该支持如下操作：
+ * <p>
+ * MyCircularQueue(k): 构造器，设置队列长度为 k 。
+ * Front: 从队首获取元素。如果队列为空，返回 -1 。
+ * Rear: 获取队尾元素。如果队列为空，返回 -1 。
+ * enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+ * deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+ * int index=(resr+length-1)%length
+ * array[index];
+ * isEmpty(): 检查循环队列是否为空。
+ * solution: 1.size属性
+ * isFull(): 检查循环队列是否已满。
+ * @Param: private int[] array;
+ * private int size;
+ * private int rear;、、下一个可用位置下标
+ * private int front;队首元素下标
+ * rear front;rear=(rear+1)%length
+ * @return:
+ */
+class MyQueue {
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+
+    }
+
+    /** Get the front element. */
+    public int peek() {
+
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+
+    }
+}
+
+/**
+ * @Description: 最小栈：设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * @Param:
+ * solution：利用两个栈：一个栈正常存入数据 另一个栈压入最小的元素（空间换时间）
+ * @return:
+ */
+
+/**
+ * @Description: 设计你的循环队列实现。 循环队列是一种线性数据结构，
+ * 其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+ *
+ * 循环队列的一个好处是我们可以利用这个队列之前用过的空间。
+ * 在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。
+ * 但是使用循环队列，我们能使用这些空间去存储新的值。
+ *
+ * 你的实现应该支持如下操作：
+ *
+ * MyCircularQueue(k): 构造器，设置队列长度为 k 。
+ * Front: 从队首获取元素。如果队列为空，返回 -1 。
+ * Rear: 获取队尾元素。如果队列为空，返回 -1 。
+ * enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+ * deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+ *           int index=(resr+length-1)%length
+ *            array[index];
+ * isEmpty(): 检查循环队列是否为空。
+ *              solution: 1.size属性
+ * isFull(): 检查循环队列是否已满。
+ * @Param: private int[] array;
+ *          private int size;
+ *          private int rear;、、下一个可用位置下标
+ *          private int front;队首元素下标
+ *          rear front;rear=(rear+1)%length
+
+ * @return:
+ */
+}
