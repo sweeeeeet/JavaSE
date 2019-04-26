c7a6ef7f89497ec8f742c6c77703cb0d0498c48f
TangLing <1158354636@qq.com>
2019-04-26 08:20:17 +0800

    usage of tomcat

---
+++ tomcat-http/src/main/java/com/github/sweeeeeet/tomcat/HelloServlet.java
@@ -0,0 +1,10 @@
+package com.github.sweeeeeet.tomcat;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Author:sweet
+ * Created:2019/4/25
+ */
+public class HelloServlet extends HttpServlet {
+}
