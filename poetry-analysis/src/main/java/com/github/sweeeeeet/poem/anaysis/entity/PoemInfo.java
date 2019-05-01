d2d42c12290f5b69add148dadc55be1f0346ecab
TangLing <1158354636@qq.com>
2019-05-01 21:20:13 +0800

    poetry-analysis project

---
+++ poetry-analysis/src/main/java/com/github/sweeeeeet/poem/anaysis/entity/PoemInfo.java
@@ -0,0 +1,16 @@
+package com.github.sweeeeeet.poem.anaysis.entity;
+
+import lombok.Data;
+
+/**
+ * Author:sweet
+ * Created:2019/4/25
+ */
+@Data
+public class PoemInfo {
+   private String title;
+   private String dynasty;
+   private String author;
+   private String content;
+
+}
