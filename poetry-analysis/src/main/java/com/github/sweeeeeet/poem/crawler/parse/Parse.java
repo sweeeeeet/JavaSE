e5d8670fe977027d30dbaf3949d44478ab9c1843
TangLing <1158354636@qq.com>
2019-05-04 08:48:24 +0800

    poetry-analysis project

---
+++ poetry-analysis/src/main/java/com/github/sweeeeeet/poem/crawler/parse/Parse.java
@@ -0,0 +1,16 @@
+package com.github.sweeeeeet.poem.crawler.parse;
+
+import com.github.sweeeeeet.poem.crawler.common.Page;
+
+/**
+ * Author:sweet
+ * Created:2019/4/25
+ */
+public interface Parse {
+    /** 
+    * @Description: 解析页面 
+    * @Param:  
+    * @return:  
+    */
+    void parse(Page page);
+}
