4b1ea8a0dc1ad1a6abdcf82ed70fefccfcdf0548
TangLing <1158354636@qq.com>
2019-05-05 08:19:30 +0800

    poetry-analysis project

---
+++ poetry-analysis/src/test/java/com/sweet/HtmlUtilTest.java
@@ -0,0 +1,16 @@
+package com.sweet;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+
+/**
+ * Author:sweet
+ * Created:2019/4/24
+ */
+public class HtmlUtilTest {
+    public static void main(String[] args) {
+//        WebClient webClient=new WebClient(BrowserVersion.CHROME){
+//
+//        }
+    }
+}
