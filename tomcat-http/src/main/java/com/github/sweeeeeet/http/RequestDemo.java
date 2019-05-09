9d25174719824e5ce34f53bcd88be07be4b0dddd
TangLing <1158354636@qq.com>
2019-05-09 15:13:21 +0800

    usage of tomcat

---
+++ tomcat-http/src/main/java/com/github/sweeeeeet/http/RequestDemo.java
@@ -0,0 +1,55 @@
+package com.github.sweeeeeet.http;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Author:sweet
+ * Created:2019/4/25
+ */
+public class RequestDemo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取请求方法
+        String method=req.getMethod();
+        //获取请求uri
+        req.getRequestURI();
+        //请求的协议版本
+        req.getProtocol();
+        //获取指定的请求头信息
+        req.getHeader("host");
+    }
+
+    @Override
+    protected long getLastModified(HttpServletRequest req) {
+        return super.getLastModified(req);
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doHead(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doOptions(req, resp);
+    }
+}
