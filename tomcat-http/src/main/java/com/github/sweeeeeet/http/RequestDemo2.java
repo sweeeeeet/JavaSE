1b1e3d0a143e36b68a8f622e05e3f8f4f0d4f97f
TangLing <1158354636@qq.com>
2019-05-11 09:10:22 +0800

    basic knowledge and usage of tomcat server

---
+++ tomcat-http/src/main/java/com/github/sweeeeeet/http/RequestDemo2.java
@@ -0,0 +1,22 @@
+package com.github.sweeeeeet.http;
+
+import com.github.sweeeeeet.tomcat.HelloServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Author:sweet
+ * Created:2019/4/25
+ */
+public class RequestDemo2  extends HelloServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取user-agent请求头
+        req.getHeader("user-agent");
+        //获取浏览器类型
+
+    }
+}
