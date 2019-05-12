6fb17efbaaaf7184db407ab657489e9bade252bf
TangLing <1158354636@qq.com>
2019-05-12 08:56:42 +0800

    checkStand project

---
+++ checkStand/src/main/java/com/sweeeeeet/Goods.java
@@ -0,0 +1,47 @@
+package com.sweeeeeet;
+
+/**
+ * Author:sweet
+ * Created:2019/1/19
+ */
+public class Goods {
+
+
+    private String name;
+    private double price;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Goods(String id,String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.id = id;
+    }
+    @Override
+    public String toString() {
+        return this.id+":"+name+":"+price;
+    }
+}
