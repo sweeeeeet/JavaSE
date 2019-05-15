91c5be58b4dc3f2b16c538274aa53496e23eb16a
TangLing <1158354636@qq.com>
2019-05-15 14:09:19 +0800

    checkStand project

---
+++ checkStand/src/main/test/java/com/sweeeeeet/SimpleOrderCerterTest.java
@@ -0,0 +1,85 @@
+package test.com.sweeeeeet; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* SimpleOrderCerter Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>05/11/2019</pre> 
+* @version 1.0 
+*/ 
+public class SimpleOrderCerterTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: addOrder(Order order) 
+* 
+*/ 
+@Test
+public void testAddOrder() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: removeOrder(Order order) 
+* 
+*/ 
+@Test
+public void testRemoveOrder() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: orderTable(String id) 
+* 
+*/ 
+@Test
+public void testOrderTable() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: ordersTable() 
+* 
+*/ 
+@Test
+public void testOrdersTable() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: storeOrders() 
+* 
+*/ 
+@Test
+public void testStoreOrders() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: loadOrders() 
+* 
+*/ 
+@Test
+public void testLoadOrders() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
