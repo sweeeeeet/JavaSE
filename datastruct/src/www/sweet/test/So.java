74acef5eeeacd7a0dc9410bdaf9140abee14e850
TangLing <1158354636@qq.com>
2019-07-24 22:28:02 +0800

    datastruct

--- datastruct/src/www/sweet/test/So.java
+++ datastruct/src/www/sweet/test/So.java
@@ -6,6 +6,26 @@ import java.util.Arrays;
  * Author:sweet
  * Created:2019/4/19
  */
+
+class Singleton {
+    private Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public Singleton getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
+
+
 //保存两个返回值
 class ReturnRoot1 {
     int used;
@@ -31,54 +51,62 @@ class Node {
 
 public class So {
 
-    public ReturnRoot1 build(String str) {
-        if (str == null) {
-            return null;
-        }
-        char[] tree = str.toCharArray();
-        if (tree.length == 0) {
-            return new ReturnRoot1(null, 0);
-        }
-        //有值
-        Node root = new Node(tree[0]);
 
-        if (tree[0] == '#') {
-            return new ReturnRoot1(null, 1);
-        } else {
-            //处理左子树
-            //ABC##DE#G##F###
-            char[] leftTree = Arrays.copyOfRange(tree, 1, tree.length);
-            ReturnRoot1 left = build(String.valueOf(leftTree));
-            //左子树递归完毕，剩余都是右子树，处理右子树
-            char[] rightTree = Arrays.copyOfRange
-                    (tree,  left.used+1, tree.length);
-            ReturnRoot1 right = build(String.valueOf(rightTree));
-            //连接根节点与左右子树
-            root.left = left.returnRoot;
-            root.right = right.returnRoot;
-            return new ReturnRoot1(root, 1 + left.used + right.used);
-        }
-    }
 
-    //中序遍历
-    public void travel(Node root) {
-        if (root == null) {
 
-        } else {
-            //中序：左 根 右
 
-            travel(root.left);
-            System.out.print(root.val + " ");
-            travel(root.right);
-        }
-    }
+
+//    public ReturnRoot1 build(String str) {
+//        if (str == null) {
+//            return null;
+//        }
+//        char[] tree = str.toCharArray();
+//        if (tree.length == 0) {
+//            return new ReturnRoot1(null, 0);
+//        }
+//        //有值
+//        Node root = new Node(tree[0]);
+//
+//        if (tree[0] == '#') {
+//            return new ReturnRoot1(null, 1);
+//        } else {
+//            //处理左子树
+//            //ABC##DE#G##F###
+//            char[] leftTree = Arrays.copyOfRange(tree, 1, tree.length);
+//            ReturnRoot1 left = build(String.valueOf(leftTree));
+//            //左子树递归完毕，剩余都是右子树，处理右子树
+//            char[] rightTree = Arrays.copyOfRange
+//                    (tree,  left.used+1, tree.length);
+//            ReturnRoot1 right = build(String.valueOf(rightTree));
+//            //连接根节点与左右子树
+//            root.left = left.returnRoot;
+//            root.right = right.returnRoot;
+//            return new ReturnRoot1(root, 1 + left.used + right.used);
+//        }
+//    }
+//
+//    //中序遍历
+//    public void travel(Node root) {
+//        if (root == null) {
+//
+//        } else {
+//            //中序：左 根 右
+//
+//            travel(root.left);
+//            System.out.print(root.val + " ");
+//            travel(root.right);
+//        }
+//    }
 
 
     public static void main(String[] args) {
-        So test = new So();
-        ReturnRoot1 root = test.build("ABC##DE#G##F###");
-        test.travel(root.returnRoot);
-        //System.out.println("".toCharArray().length);
+//        So test = new So();
+//        ReturnRoot1 root = test.build("ABC##DE#G##F###");
+//        test.travel(root.returnRoot);
+//        //System.out.println("".toCharArray().length);
     }
+
+
+
 }
 
