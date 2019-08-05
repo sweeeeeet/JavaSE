eab55be1a65257a1ccd106963d73271727c04e8b
TangLing <1158354636@qq.com>
2019-08-05 23:54:51 +0800

    nowcode

--- datastruct/src/www/sweet/test/Main.java
+++ datastruct/src/www/sweet/test/Main.java
@@ -1,25 +1,39 @@
-import  java.util.*;
-public  class Main{
-    public static  void main(String[] args){
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int t= sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            int[] array=new int[n];
-            for(int i=0;i<n;i++){
-                array[i]=sc.nextInt();
+        while(sc.hasNext()){
+            String str1=sc.nextLine();
+            String str2=sc.nextLine();
+            if(str1==null||str2==null||str1.length()==0||str2.length()==0){
+                return;
             }
-            for(int i=0;i<n;i++){
-
-                int front=(n-i-1)%n;
-                int next=(n-i+1)%n;
-                if(array[front]+array[next]<array[i]){
-                    System.out.println("No");
-                    break;
+            char[] ch1=str1.toCharArray();
+            char[] ch2=str2.toCharArray();
+            StringBuffer sb=new StringBuffer();
+            StringBuffer res=new StringBuffer();
+            for(int i=0;i<ch1.length;i++){
+                //利用动态规划的思想，同时遍历两个字符串
+                int index=i;
+                for(int j=0;j<ch2.length;j++){
+                    //首先保证在第二个字符串中不越界，并且两个字符相等，那么进入指针前进，并且追加到公共记录中
+                    if(index<ch1.length&&ch1[index]==ch2[j]){
+                        index++;
+                        sb.append(ch2[j]);
+                    }else{
+                        //判断是否是最长字符串
+                        if(sb.length()>res.length()){
+                            res=sb;
+                        }
+                        //如果两个字符不相等，指针不前进，将公共字符串记录更新为空字符串
+                        index=i;
+                        sb=new StringBuffer("");
+                    }
+                }
+                if(sb.length()>res.length()){
+                    res=sb;
                 }
             }
-            System.out.println("Yes");
-
         }
     }
 }
